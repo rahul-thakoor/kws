8214f1d92e64b53e3844ae42a7532d96ebd3bbf7
Rahul Thakoor <rahul-thakoor@users.noreply.github.com>
2019-02-25 09:25:00 +0400

    Go mongodb (#5)
    * crud complete

--- go_mongodb_crud/demo_app/crud.go
+++ go_mongodb_crud/demo_app/crud.go
@@ -27,13 +27,37 @@ func main() {
 	}
 	log.Println("connection successful")
 
+
+	// create a Module type representing a particular course module
+	type Module struct{
+		Id string
+		Title string
+		ECTS int
+		Description string
+	}
+
+	// Get collection
+	collection := client.Database("supinfo-kws-demo").Collection("ASc1 Modules")
+
+
+	deleteResult, err := collection.DeleteOne(ctx, bson.D{{"id", "2WEB"}})
+
 	collection := client.Database("supinfo-kws-demo").Collection("Assignments")
 	insertResult, err := collection.InsertOne(ctx, bson.M{"ID":"MP1", "Description":"Puzzler- Use HTML5 to create a mini puzzle game", "Module":"1WEB" , "Deadline":"25-09-2018" })
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
+
+	log.Println("No. of deleted document(s) : ", deleteResult.DeletedCount)
+
+	// close connection
+	client.Disconnect(ctx)
+
+
 	log.Println("Inserted a single document: ", insertResult.InsertedID)
 	// close connection
 	client.Disconnect(ctx)
+
 }
